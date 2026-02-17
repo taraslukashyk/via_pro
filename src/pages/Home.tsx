2cc4c2baecc494ba3f2e8feebceb3f16d0122d19
taraslukashyk <taraslukashik@gmail.com>
2026-02-18 01:02:06 +0200

    збільшив напис VIA PRO на мобільних пристроях до 20vw

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -45,7 +45,7 @@ const Home: React.FC = () => {
                         initial={{ opacity: 0, y: 100 }}
                         animate={{ opacity: 1, y: 0 }}
                         transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
-                        className="text-[12vw] leading-none font-bold tracking-tighter text-accent"
+                        className="text-[20vw] md:text-[12vw] leading-none font-bold tracking-tighter text-accent"
                     >
                         VIA PRO
                     </motion.h1>
